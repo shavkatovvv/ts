77b17e68c3522fa9e1e69a36630042cde6dee6d1
shavkatovvv <shavkatovshohruh845@gmail.com>
2024-11-26 06:15:25 +0500

    finish

---
+++ main.js
@@ -0,0 +1,43 @@
+"use strict";
+// const a: string = "hello";
+// const b: string = "there";
+// const x = (a: string, b: string) => {
+//     let first = a.substring(1);
+//     let second = b.substring(1);
+//     return first + second;
+// };
+// console.log(x(a, b));
+// let a: string | null = prompt("Enter a string");
+// let num: number = Number(prompt("Enter a number"));
+// let b: string = "";
+// if (a != null && num != null) {
+//     for (let i = 0; i < num; i++) {
+//         b += a[i];
+//     }
+// }
+// console.log(b);
+// const a: string | null = prompt("Enter a string");
+// const bool: boolean = prompt("Enter 'true' or 'false'") === "true";
+// const x = () => {
+//     if (a != null) {
+//         if (bool) {
+//             console.log(a[0]);
+//         } else {
+//             console.log(a[a.length - 1]);
+//         }
+//     } else {
+//         console.log("null");
+//     }
+// };
+// x();
+// const a: string | null = prompt("Enter a string");
+// const x = () => {
+//     if (a !== null && a.length >= 2 && a.slice(-2) === "ly") {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// };
+// x();
+const a = "helloblue";
+const x = (a) => { };
